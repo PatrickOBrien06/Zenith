43c7af37882fb9c74b18747570b2706ee0e34b12
PatrickOB <124152931+PatrickOBrien06@users.noreply.github.com>
2023-12-30 22:36:57 -0330

    issue: Corrosponding set data to exercise not displaying

--- website/training.py
+++ website/training.py
@@ -15,32 +15,42 @@ training = Blueprint("training", __name__, template_folder="templates")
 def home():
     schedules = Schedule.query.filter_by(user_id=current_user.id).all()
     exercises = Exercise.query.filter_by(user_id=current_user.id).all()
-    sets = Set.query.filter_by(user_id=current_user.id)
+    sets = Set.query.filter_by(user_id=current_user.id).all()
+    
+    setx = Set.query.all()
 
-    return render_template("index.html", schedules=schedules, exercises=exercises, sets=sets)
+    for set in setx:
+        print(set.weight)
+        print(set.reps)
+
+    return render_template("index.html", schedules=schedules, exercises=exercises, sets=sets, current_user=current_user)
 
 # Create Schedule
 @training.route("/create_schedule", methods=["POST", "GET"])
 @login_required
 def create_schedule():
     if request.method == "POST":
+        index = 0
         schedule_name = request.form.get("schedule_name")
-        exercise_name = request.form.get("exercise_name")
-        set_weight = request.form.get("set_weight")
-        set_reps = request.form.get("set_reps")
+        exercise_names = request.form.getlist("exercise_name")
+        sets_weight = request.form.getlist("set_weight")
+        sets_reps = request.form.getlist("set_reps")
         
-        user = User.query.filter_by(email=current_user.email).first()
-
-        schedule = Schedule(schedule_name=schedule_name, user_id=user.id, date_created=datetime.utcnow())
+        schedule = Schedule(schedule_name=schedule_name, date_created=datetime.utcnow(), user_id=current_user.id)
         db.session.add(schedule)
         db.session.commit()
-
-        exercise = Exercise(exercise_name=exercise_name, schedule_id=schedule.id, user_id=user.id)
-        db.session.add(exercise)
-        db.session.commit()
-
-        set_data = Set(reps=int(set_reps), weight=float(set_weight), exercise_id=exercise.id, user_id=user.id)
-        db.session.add(set_data)
-        db.session.commit()
+        # Add the controls and loop through the list
+        for exercise_name in exercise_names:
+            exercise = Exercise(exercise_name=exercise_name, schedule_id=schedule.id, user_id=current_user.id)
+            db.session.add(exercise)
+            db.session.commit()
+            for set_weight in sets_weight:
+                index += 1
+                for set_reps in sets_reps:
+                    set = Set(reps=set_reps[index], weight=set_weight[index], exercise_id=exercise.id, user_id=current_user.id)
+                    db.session.add(set)
+                    db.session.commit()
+                    
+            
 
     return render_template("create_schedule.html")
