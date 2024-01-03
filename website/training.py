7d78676664959f091003ea5c0c36556f3a54d206
PatrickOB <124152931+PatrickOBrien06@users.noreply.github.com>
2024-01-03 19:12:34 -0330

    fixed: posting issue fixed, schedule creation implemented

--- website/training.py
+++ website/training.py
@@ -13,15 +13,11 @@ training = Blueprint("training", __name__, template_folder="templates")
 @training.route("/home", methods=['POST', 'GET'])
 @login_required
 def home():
+
+    # Filter for rendering in home page
     schedules = Schedule.query.filter_by(user_id=current_user.id).all()
     exercises = Exercise.query.filter_by(user_id=current_user.id).all()
     sets = Set.query.filter_by(user_id=current_user.id).all()
-    
-    setx = Set.query.all()
-
-    for set in setx:
-        print(set.weight)
-        print(set.reps)
 
     return render_template("index.html", schedules=schedules, exercises=exercises, sets=sets, current_user=current_user)
 
@@ -33,30 +29,28 @@ def create_schedule():
     if request.method == "POST":
         schedule_name = request.form.get("schedule_name")
         exercise_names = request.form.getlist("exercise_name[]")
-        sets_weight = request.form.getlist("set_weight[]")
-        sets_reps = request.form.getlist("set_reps[]")
         
-        schedule = Schedule(schedule_name=schedule_name, date_created=datetime.utcnow(), user_id=current_user.id)
+        schedule = Schedule(schedule_name=schedule_name, user_id=current_user.id)
         db.session.add(schedule)
         db.session.commit()
 
-        # Loop through each exercise
-        for index, exercise_name in enumerate(exercise_names):
+        exerciseCounter = 1
+
+        for exercise_name in exercise_names:
             exercise = Exercise(exercise_name=exercise_name, schedule_id=schedule.id, user_id=current_user.id)
             db.session.add(exercise)
             db.session.commit()
 
-            # Extract sets for the current exercise based on index and the total number of exercises
-            sets_per_exercise = len(sets_weight) // len(exercise_names)
-            sets_start = index * sets_per_exercise
-            sets_end = sets_start + sets_per_exercise
-            exercise_sets_weight = sets_weight[sets_start:sets_end]
-            exercise_sets_reps = sets_reps[sets_start:sets_end]
+            sets_weight = request.form.getlist(f"set_weight_{exerciseCounter}[]")
+            sets_reps = request.form.getlist(f"set_reps_{exerciseCounter}[]")
 
-            # Link sets to the current exercise
-            for set_weight, set_reps in zip(exercise_sets_weight, exercise_sets_reps):
-                set_entry = Set(reps=set_reps, weight=set_weight, exercise_id=exercise.id, user_id=current_user.id)
-                db.session.add(set_entry)
+            exerciseCounter += 1
+
+            for setCounter in range(len(sets_weight)):
+                set_weight = sets_weight[setCounter]
+                set_reps = sets_reps[setCounter]
+                set = Set(reps=set_reps, weight=set_weight, exercise_id=exercise.id, user_id=current_user.id)
+                db.session.add(set)
                 db.session.commit()
 
     return render_template("create_schedule.html")
